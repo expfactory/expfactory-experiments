93b350599d64f1376350a0b28c30f0e091d246df
vsoch <vsochat@stanford.edu>
2015-11-25 16:18:06 -0800

    removing old data, this will need to be fixed/updated

--- .script/generate_index.py
+++ .script/generate_index.py
@@ -4,9 +4,9 @@ import os
 import pandas
 import json
 
-myexperiments = find_directories("../psiturk-experiments")
-output_file = os.path.abspath("../psiturk-experiments/index.html")
-data_folder = os.path.abspath("../psiturk-experiments/.data")
+myexperiments = find_directories("../")
+output_file = os.path.abspath("../index.html")
+data_folder = os.path.abspath("../.data")
 
 # Let's make a dataframe of valid experiments
 fields = [f[0] for f in get_validation_fields()]
@@ -28,7 +28,7 @@ table = valid.to_html(index=None,classes="table table-striped table-bordered")
 table = table.replace('class="dataframe',' id="chart" class="dataframe')  
 
 # Read in template
-template = "".join(open("../psiturk-experiments/.script/index.html","rb").readlines())
+template = "".join(open("index.html","rb").readlines())
 template = template.replace("[[SUB_TABLE_SUB]]",table)
 filey = open(output_file,"wb")
 filey.writelines(template)
@@ -41,6 +41,6 @@ def save_pretty_json(outfile,myjson):
     filey.close()
 
 # Finally, save other versions of updated metadata for people to use
-save_pretty_json("%s/psiturk-experiments.json" %(data_folder),json.loads(valid.to_json(orient="records")))
-valid.to_csv("%s/psiturk-experiments.tsv" %(data_folder),sep="\t",index=None)
-valid.to_pickle("%s/psiturk-experiments.pkl" %(data_folder))
+save_pretty_json("%s/efactory-experiments.json" %(data_folder),json.loads(valid.to_json(orient="records")))
+valid.to_csv("%s/efactory-experiments.tsv" %(data_folder),sep="\t",index=None)
+valid.to_pickle("%s/efactory-experiments.pkl" %(data_folder))
