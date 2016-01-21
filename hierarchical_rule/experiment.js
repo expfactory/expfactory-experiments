694a40262cefbbfe395746c6fb04fa44abc2ffa8
ian <ianeisenberg90@gmail.com>
2016-01-20 17:30:37 -0800

    fixed bugs in hierarchical task

--- hierarchical_rule/experiment.js
+++ hierarchical_rule/experiment.js
@@ -46,7 +46,7 @@ var getFlatData = function() {
 /* ************************************ */
 var exp_len = 200 //number of trials per rule-set
 var flat_first = 0//  Math.floor(Math.random())
-var path_source = 'static/experiments/hierarchical_rule/images/'
+var path_source = '/static/experiments/hierarchical_rule/images/'
 var stim_prefix = '<div class = centerbox><div class = stimBox><img class = hierarchicalStim src ='
 var border_prefix = '<img class = hierarchicalBorder src ='
 var prompt_prefix = '<img class = hierarchicalPrompt src ='
@@ -110,7 +110,8 @@ var welcome_block = {
   timing_post_trial: 0,
   on_finish: function() {
   	$('body').css('background','black')
-};
+  }
+}
 
 var instructions_block = {
   type: 'poldrack-instructions',
@@ -125,7 +126,7 @@ var instructions_block = {
 
 var end_block = {
   type: 'text',
-  text: '<div class = centerbox><p class = center-"white-text block-text">Thanks for completing this task!</p><p class = center-"white-text block-text">Press <strong>enter</strong> to begin.</p></div>',
+  text: '<div class = centerbox><p class = "white-text center-block-text">Thanks for completing this task!</p><p class = "white-text center-block-text">Press <strong>enter</strong> to begin.</p></div>',
   cont_key: [13],
   timing_post_trial: 0,
   on_finish: function() {
@@ -136,7 +137,7 @@ var end_block = {
 
 var start_test_block = {
   type: 'text',
-  text: '<div class = centerbox><p class = "white-text block-text">We will now start the test.</p><p class = center-"white-text block-text">Press <strong>enter</strong> to begin.</p></div>',
+  text: '<div class = centerbox><p class = "white-text center-block-text">We will now start the test.</p><p class = "white-text center-block-text">Press <strong>enter</strong> to begin.</p></div>',
   cont_key: [13],
   timing_post_trial: 1000
 };
