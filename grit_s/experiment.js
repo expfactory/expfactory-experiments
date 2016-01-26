4ec93aa0f1d0dbdd93ceb8b80997f18f5107880b
Zeynep Enkavi <zeynepenkavi@gmail.com>
2016-01-26 15:01:54 -0800

    Added grit_s with new plugin and layout.

---
+++ grit_s/experiment.js
@@ -0,0 +1,75 @@
+
+/* ************************************ */
+/* Define helper functions */
+/* ************************************ */
+function getDisplayElement () {
+    $('<div class = display_stage_background></div>').appendTo('body')
+    return $('<div class = display_stage></div>').appendTo('body')
+}
+
+function fillArray(value, len) {
+  if (len == 0) return [];
+  var a = [value];
+  while (a.length * 2 <= len) a = a.concat(a);
+  if (a.length < len) a = a.concat(a.slice(0, len - a.length));
+  return a;
+}
+/* ************************************ */
+/* Define experimental variables */
+/* ************************************ */
+
+/* ************************************ */
+/* Set up jsPsych blocks */
+/* ************************************ */
+
+/* define static blocks */
+var welcome_block = {
+  type: 'text',
+  text: '<div class = centerbox><p class = center-block-text>Welcome to the experiment. Press <strong>enter</strong> to begin.</p></div>',
+  cont_key: [13]
+};
+
+var instructions_block = {
+  type: 'instructions',
+  pages: [
+    '<div class = centerbox><p class = block-text>For each of the following statements, please indicate the descriptive they are of you.<br><br> Press <strong>enter</strong> to begin.</p></div>',
+  ],
+  key_forward: 13,
+  allow_backwards: false
+};
+
+var opts = ["Not at all like me - 1", "2", "3", "4", "Very much like me - 5"]
+
+var all_pages = [["I often set a goal but later choose to pursue a different one.","New ideas and projects sometimes distract me from previous ones.","I have been obsessed with a certain idea or project for a short time but later lost interest.","I have diﬂiculty maintaining my focus on projects that take more than a few months to complete.","I ﬁnish whatever I begin.","Setbacks don’t discourage me.","I am a hard worker.","I am diligent."]]
+
+var all_options = [fillArray(opts, 8)]
+
+var score_scale = {"Not at all like me - 1":1, "2":2, "3":3, "4":4, "Very much like me - 5":5}
+
+var survey_block = {
+  type: "poldrack-survey-multi-choice",
+  horizontal: true,
+  preamble: '',
+  pages: all_pages,
+  options: all_options,
+  scale: score_scale,
+  show_clickable_nav: true,
+  allow_backward: true,
+  required: [fillArray(true, 8)],
+  reverse_score: [[false, false, false, true, false, false, false, false]],
+};
+
+var end_block = {
+  type: 'text',
+  timing_response: 60000,
+  text: '<div class = centerbox><p class = center-block-text>Congratulations for completing this task!</p><p class = center-block-text>Press <strong>enter</strong> to continue.</p></div>',
+  cont_key: [13]
+};
+
+
+//Set up experiment
+var grit_s_experiment = []
+grit_s_experiment.push(welcome_block);
+grit_s_experiment.push(instructions_block);
+grit_s_experiment.push(survey_block);
+grit_s_experiment.push(end_block);
