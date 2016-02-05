8ba621ae74b1b59313316ba096d5e01816ae288b
ian <ianeisenberg90@gmail.com>
2016-02-04 22:27:35 -0800

    fixed infinite instruction loop in ANT, fixed undefined choice in hierarchical rule

--- attention_network_task/experiment.js
+++ attention_network_task/experiment.js
@@ -279,9 +279,10 @@ var instruction_node = {
 	/* This function defines stopping criteria */
 	loop_function: function(data) {
 		for (i = 0; i < data.length; i++) {
-			if ((data[i].trial_id == 'poldrack-instructions') && (data[i].rt != -1)) {
+			if ((data[i].trial_type == 'poldrack-instructions') && (data[i].rt != -1)) {
 				rt = data[i].rt
-				sumInstructTime = sumInstructTime + rt
+				sumInstructTime += rt
+				console.log(sumInstructTime)
 			}
 		}
 		if (sumInstructTime <= instructTimeThresh * 1000) {
