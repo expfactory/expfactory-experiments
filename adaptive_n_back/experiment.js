b2063de4721e6cc309783c5763264d1bd2570797
ian <ianeisenberg90@gmail.com>
2016-04-13 12:37:41 -0700

    updated adaptive_n_back

--- adaptive_n_back/experiment.js
+++ adaptive_n_back/experiment.js
@@ -1,45 +1,6 @@
 /* ************************************ */
 /* Define helper functions */
 /* ************************************ */
-// Class to track focus shifts during experiment
-var focus_tracker = function(win) {
-	var self = this
-    this.shift_away = 0;
-
-  this.add_shift = function() {
-  	this.shift_away += 1
-  },
-
-  this.get_shifts = function() {
-    return this.shift_away;
-  },
-
-  this.reset = function() {
-  	this.shift_away = 0
-  },
-
-  $(win).blur(function(){
-	  self.add_shift()
-	});
-}
-
-var focuser = new focus_tracker(window)
-
-
-function getDisplayElement() {
-	$('<div class = display_stage_background></div>').appendTo('body')
-	return $('<div class = display_stage></div>').appendTo('body')
-}
-
-function addID(exp_id) {
-  var isFullScreen = document.mozFullScreen || document.webkitIsFullScreen || (!window.screenTop && !window.screenY) 
-	jsPsych.data.addDataToLastTrial({
-		exp_id: exp_id,
-		full_screen: isFullScreen,
-		focus_shifts: focuser.get_shifts()
-	})
-}
-
 function evalAttentionChecks() {
 	var check_percent = 1
 	if (run_attention_checks) {
