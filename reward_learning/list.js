adab0d29f07560b0d3a8d39fc7234f9c83b15c5d
gewimmer <gelliottwimmer@stanford.edu>
2016-07-13 16:10:49 -0700

    Add files via upload

---
+++ reward_learning/list.js
@@ -0,0 +1,108 @@
+
+
+var picArray = [];
+var subjcodeArray = [];
+
+picArray[0] = ["static/experiments/reward_learning/images/1.png",
+	"static/experiments/reward_learning/images/2.png",
+	"static/experiments/reward_learning/images/3.png",
+	"static/experiments/reward_learning/images/4.png",
+	"static/experiments/reward_learning/images/5.png",
+	"static/experiments/reward_learning/images/6.png",
+	"static/experiments/reward_learning/images/7.png",
+	"static/experiments/reward_learning/images/8.png",
+  "static/experiments/reward_learning/images/101.png",
+	"static/experiments/reward_learning/images/102.png"
+];
+subjcodeArray[0] = "Default!";
+
+picArray[250] = ["static/experiments/reward_learning/images/1.png",
+	"static/experiments/reward_learning/images/2.png",
+	"static/experiments/reward_learning/images/1.png",
+	"static/experiments/reward_learning/images/2.png",
+	"static/experiments/reward_learning/images/1.png",
+	"static/experiments/reward_learning/images/2.png",
+	"static/experiments/reward_learning/images/1.png",
+	"static/experiments/reward_learning/images/2.png",
+  "static/experiments/reward_learning/images/101.png",
+	"static/experiments/reward_learning/images/102.png"
+];
+subjcodeArray[250] = "rainbow";
+
+picArray[251] = ["static/experiments/reward_learning/images/3.png",
+	"static/experiments/reward_learning/images/4.png",
+	"static/experiments/reward_learning/images/3.png",
+	"static/experiments/reward_learning/images/4.png",
+	"static/experiments/reward_learning/images/3.png",
+	"static/experiments/reward_learning/images/4.png",
+	"static/experiments/reward_learning/images/3.png",
+	"static/experiments/reward_learning/images/4.png",
+  "static/experiments/reward_learning/images/101.png",
+	"static/experiments/reward_learning/images/102.png"
+];
+subjcodeArray[251] = "blackfish";
+
+picArray[252] = ["static/experiments/reward_learning/images/5.png",
+	"static/experiments/reward_learning/images/6.png",
+	"static/experiments/reward_learning/images/5.png",
+	"static/experiments/reward_learning/images/6.png",
+	"static/experiments/reward_learning/images/5.png",
+	"static/experiments/reward_learning/images/6.png",
+	"static/experiments/reward_learning/images/5.png",
+	"static/experiments/reward_learning/images/6.png",
+  "static/experiments/reward_learning/images/101.png",
+	"static/experiments/reward_learning/images/102.png"
+];
+subjcodeArray[252] = "bicycle";
+
+picArray[253] = ["static/experiments/reward_learning/images/7.png",
+	"static/experiments/reward_learning/images/8.png",
+	"static/experiments/reward_learning/images/7.png",
+	"static/experiments/reward_learning/images/8.png",
+	"static/experiments/reward_learning/images/7.png",
+	"static/experiments/reward_learning/images/8.png",
+	"static/experiments/reward_learning/images/7.png",
+	"static/experiments/reward_learning/images/8.png",
+  "static/experiments/reward_learning/images/101.png",
+	"static/experiments/reward_learning/images/102.png"
+];
+subjcodeArray[253] = "python";
+
+picArray[254] = ["static/experiments/reward_learning/images/9.png",
+	"static/experiments/reward_learning/images/10.png",
+	"static/experiments/reward_learning/images/9.png",
+	"static/experiments/reward_learning/images/10.png",
+	"static/experiments/reward_learning/images/9.png",
+	"static/experiments/reward_learning/images/10.png",
+	"static/experiments/reward_learning/images/9.png",
+	"static/experiments/reward_learning/images/10.png",
+  "static/experiments/reward_learning/images/101.png",
+	"static/experiments/reward_learning/images/102.png"
+];
+subjcodeArray[254] = "fit";
+
+picArray[255] = ["static/experiments/reward_learning/images/1.png",
+	"static/experiments/reward_learning/images/2.png",
+	"static/experiments/reward_learning/images/1.png",
+	"static/experiments/reward_learning/images/2.png",
+	"static/experiments/reward_learning/images/1.png",
+	"static/experiments/reward_learning/images/2.png",
+	"static/experiments/reward_learning/images/1.png",
+	"static/experiments/reward_learning/images/2.png",
+  "static/experiments/reward_learning/images/101.png",
+	"static/experiments/reward_learning/images/102.png"
+];
+subjcodeArray[255] = "ruby";
+
+picArray[256] = ["static/experiments/reward_learning/images/3.png",
+	"static/experiments/reward_learning/images/4.png",
+	"static/experiments/reward_learning/images/3.png",
+	"static/experiments/reward_learning/images/4.png",
+	"static/experiments/reward_learning/images/3.png",
+	"static/experiments/reward_learning/images/4.png",
+	"static/experiments/reward_learning/images/3.png",
+	"static/experiments/reward_learning/images/4.png",
+  "static/experiments/reward_learning/images/101.png",
+	"static/experiments/reward_learning/images/102.png"
+];
+subjcodeArray[255] = "thecity";
