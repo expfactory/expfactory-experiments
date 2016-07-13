7a6960a53dbb237568986e7be79198216240888c
Jamie Li <jamieli@psy-w80061za5ru.stanford.edu>
2016-07-12 19:15:23 -0700

    simple reaction time trials lengthen, add aversive message, have not modified json yet, blocks must still not end when subjects makes a response(jamie fix this)

--- simple_reaction_time/experiment.js
+++ simple_reaction_time/experiment.js
@@ -45,15 +45,64 @@ var get_trial_time = function() {
   if (gap > 4500) {
     gap = 4500
   }
-  return gap + 2000;
+  if (flag_curr_trial == 1){
+  gap = 3000
+  return gap
+  }else{
+  return gap;
+  }
+}
+
+var get_trial_time_practice = function() {
+  // ref: https://gist.github.com/nicolashery/5885280
+  function randomExponential(rate, randomUniform) {
+    // http://en.wikipedia.org/wiki/Exponential_distribution#Generating_exponential_variates
+    rate = rate || 1;
+
+    // Allow to pass a random uniform value or function
+    // Default to Math.random()
+    var U = randomUniform;
+    if (typeof randomUniform === 'function') U = randomUniform();
+    if (!U) U = Math.random();
+
+    return -Math.log(U) / rate;
+  }
+  gap = randomExponential(1)*1000
+  if (gap > 4500) {
+    gap = 4500
+  }
+  return gap + 2000
+}
+var getGap = function(){
+	return gap
+}
+
+var get_message = function(){
+	if(flag_curr_trial == 1){
+		return fast_rt_message
+	}else{
+		return '<div class = centerbox></div>'
+	}	
 }
 
 /* Append gap and current trial to data and then recalculate for next trial*/
 var appendData = function() {
-  jsPsych.data.addDataToLastTrial({
-    trial_num: current_trial
-  })
-  current_trial = current_trial + 1
+	var global_trial = jsPsych.progress().current_trial_global
+	curr_rt = jsPsych.data.getDataByTrialIndex(global_trial).rt
+	
+	if(curr_rt < 125 && curr_rt != -1){
+		fast_rt_flags +=1
+	}
+	
+	if(fast_rt_flags > 4 && curr_rt < 125 && curr_rt != -1){
+		flag_curr_trial = 1
+	}
+	
+	
+	jsPsych.data.addDataToLastTrial({
+    	trial_num: current_trial
+ 	})
+  	current_trial = current_trial + 1
 }
 
 var getInstructFeedback = function() {
@@ -74,9 +123,10 @@ var experiment_len = 50
 var gap = 0
 var current_trial = 0
 var stim = '<div class = shapebox><div id = cross></div></div>'
-
-
-
+var fast_rt_flags = 0;
+var curr_rt = ''
+var fast_rt_message = '<div class = centerbox><p class = block-text>We have detected a number of trials where the reaction time was implausibly fast.  Please make sure that you hit the spacebar <strong> once</strong>, as quickly as possible <strong>only when the large X appears</strong></p></div>'
+var flag_curr_trial = 0;
 /* ************************************ */
 /* Set up jsPsych blocks */
 /* ************************************ */
@@ -105,6 +155,19 @@ var end_block = {
   timing_post_trial: 0,
   on_finish: assessPerformance
 };
+
+var wait_block = {
+  type: 'poldrack-text',
+  data: {
+    trial_id: "end",
+    exp_id: 'simple_reaction_time'
+  },
+  timing_response: 180000,
+  text: '<div class = centerbox><p class = block-text>Take a break!  Press <strong> enter</strong> to continue.</p></div>',
+  cont_key: [13],
+  timing_post_trial: 0,
+};
+
 
 var feedback_instruct_text =
   'Welcome to the experiment. This experiment will take about 3 minutes. Press <strong>enter</strong> to begin.'
@@ -167,6 +230,8 @@ var start_practice_block = {
   timing_post_trial: 1000
 };
 
+
+
 var start_test_block = {
   type: 'poldrack-text',
   data: {
@@ -185,6 +250,8 @@ var reset_block = {
   },
   func: function() {
     current_trial = 0
+    flag_curr_trial = 0
+    fast_rt_flags = 0
   },
   timing_post_trial: 0
 }
@@ -195,7 +262,7 @@ var practice_block = {
   stimulus: stim,
   timing_post_trial: 0,
   timing_stim: 2000,
-  timing_response: get_trial_time,
+  timing_response: get_trial_time_practice,
   response_ends_trial: false,
   is_html: true,
   data: {
@@ -207,11 +274,11 @@ var practice_block = {
 };
 
 /* define test block */
-var test_block = {
+var test_block1 = {
   type: 'poldrack-single-stim',
   stimulus: stim,
   timing_stim: 2000,
-  timing_response: get_trial_time,
+  timing_response: 2000,
   timing_post_trial: 0,
   response_ends_trial: false,
   is_html: true,
@@ -221,6 +288,23 @@ var test_block = {
   },
   choices: [32],
   on_finish: appendData,
+};
+
+var test_block2 = {
+  type: 'poldrack-single-stim',
+  stimulus: get_message,
+  timing_stim: get_trial_time,
+  timing_response: getGap,
+  timing_post_trial: 0,
+  response_ends_trial: false,
+  is_html: true,
+  data: {
+    trial_id: "stim",
+    exp_stage: "test-gap-message"
+  },
+  on_finish: function(){
+  flag_curr_trial = 0;
+  }
 };
 
 /* create experiment definition array */
@@ -233,8 +317,15 @@ for (var i = 0; i < practice_len; i++) {
 }
 simple_reaction_time_experiment.push(reset_block)
 simple_reaction_time_experiment.push(start_test_block);
-for (var i = 0; i < experiment_len; i++) {
-  simple_reaction_time_experiment.push(test_block);
+
+for(var b = 0; b < 3; b++){
+	for (var i = 0; i < experiment_len; i++) {
+    	simple_reaction_time_experiment.push(test_block1);
+    	simple_reaction_time_experiment.push(test_block2);
+	}
+	simple_reaction_time_experiment.push(wait_block)
+	simple_reaction_time_experiment.push(reset_block)
 }
+
 simple_reaction_time_experiment.push(post_task_block)
 simple_reaction_time_experiment.push(end_block);
