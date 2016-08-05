2c7cb4587cd0f82570be10bdfbfc4b6fb938b26b
gewimmer <gelliottwimmer@stanford.edu>
2016-08-05 11:27:08 -0700

    Updated list.js with new participant stim lists

--- reward_learning/list.js
+++ reward_learning/list.js
@@ -465,3 +465,91 @@ picArray[321] = ["static/experiments/test_task/images/24.png",
 ];
 subjcodeArray[321] = "airplane";
 
+
+picArray[328] = ["static/experiments/test_task/images/16.png",
+  "static/experiments/test_task/images/37.png",
+  "static/experiments/test_task/images/32.png",
+  "static/experiments/test_task/images/12.png",
+  "static/experiments/test_task/images/9.png",
+  "static/experiments/test_task/images/1.png",
+  "static/experiments/test_task/images/34.png",
+  "static/experiments/test_task/images/25.png",
+  "static/experiments/test_task/images/101.png",
+	"static/experiments/test_task/images/102.png"
+];
+subjcodeArray[328] = "cabernet";
+
+
+picArray[329] = ["static/experiments/test_task/images/11.png",
+  "static/experiments/test_task/images/1.png",
+  "static/experiments/test_task/images/19.png",
+  "static/experiments/test_task/images/33.png",
+  "static/experiments/test_task/images/16.png",
+  "static/experiments/test_task/images/23.png",
+  "static/experiments/test_task/images/28.png",
+  "static/experiments/test_task/images/17.png",
+  "static/experiments/test_task/images/101.png",
+	"static/experiments/test_task/images/102.png"
+];
+subjcodeArray[329] = "monkey";
+
+
+picArray[330] = ["static/experiments/test_task/images/18.png",
+  "static/experiments/test_task/images/5.png",
+  "static/experiments/test_task/images/34.png",
+  "static/experiments/test_task/images/37.png",
+  "static/experiments/test_task/images/29.png",
+  "static/experiments/test_task/images/30.png",
+  "static/experiments/test_task/images/21.png",
+  "static/experiments/test_task/images/19.png",
+  "static/experiments/test_task/images/101.png",
+	"static/experiments/test_task/images/102.png"
+];
+subjcodeArray[330] = "television";
+
+
+picArray[337] = ["static/experiments/test_task/images/22.png",
+  "static/experiments/test_task/images/21.png",
+  "static/experiments/test_task/images/31.png",
+  "static/experiments/test_task/images/13.png",
+  "static/experiments/test_task/images/29.png",
+  "static/experiments/test_task/images/10.png",
+  "static/experiments/test_task/images/11.png",
+  "static/experiments/test_task/images/20.png",
+  "static/experiments/test_task/images/101.png",
+	"static/experiments/test_task/images/102.png"
+];
+subjcodeArray[337] = "berkeley";
+
+
+picArray[338] = ["static/experiments/test_task/images/24.png",
+  "static/experiments/test_task/images/27.png",
+  "static/experiments/test_task/images/33.png",
+  "static/experiments/test_task/images/11.png",
+  "static/experiments/test_task/images/19.png",
+  "static/experiments/test_task/images/26.png",
+  "static/experiments/test_task/images/29.png",
+  "static/experiments/test_task/images/25.png",
+  "static/experiments/test_task/images/101.png",
+	"static/experiments/test_task/images/102.png"
+];
+subjcodeArray[338] = "scooter";
+
+
+picArray[340] = ["static/experiments/test_task/images/13.png",
+  "static/experiments/test_task/images/34.png",
+  "static/experiments/test_task/images/4.png",
+  "static/experiments/test_task/images/17.png",
+  "static/experiments/test_task/images/23.png",
+  "static/experiments/test_task/images/36.png",
+  "static/experiments/test_task/images/10.png",
+  "static/experiments/test_task/images/14.png",
+  "static/experiments/test_task/images/101.png",
+	"static/experiments/test_task/images/102.png"
+];
+subjcodeArray[340] = "school";
+
+
+
+
+
