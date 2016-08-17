12bce85691d1f6ef9ba15ed7b658464371ea1f4f
gewimmer <gelliottwimmer@stanford.edu>
2016-08-17 12:04:19 -0700

    Added stimlists for new participants to list.js

--- reward_learning/list.js
+++ reward_learning/list.js
@@ -550,6 +550,114 @@ picArray[340] = ["static/experiments/test_task/images/13.png",
 subjcodeArray[340] = "school";
 
 
+picArray[349] = ["static/experiments/test_task/images/26.png",
+  "static/experiments/test_task/images/24.png",
+  "static/experiments/test_task/images/7.png",
+  "static/experiments/test_task/images/14.png",
+  "static/experiments/test_task/images/31.png",
+  "static/experiments/test_task/images/37.png",
+  "static/experiments/test_task/images/35.png",
+  "static/experiments/test_task/images/11.png",
+  "static/experiments/test_task/images/101.png",
+	"static/experiments/test_task/images/102.png"
+];
+subjcodeArray[349] = "sandi";
 
 
+picArray[336] = ["static/experiments/test_task/images/14.png",
+  "static/experiments/test_task/images/27.png",
+  "static/experiments/test_task/images/29.png",
+  "static/experiments/test_task/images/38.png",
+  "static/experiments/test_task/images/18.png",
+  "static/experiments/test_task/images/15.png",
+  "static/experiments/test_task/images/23.png",
+  "static/experiments/test_task/images/28.png",
+  "static/experiments/test_task/images/101.png",
+	"static/experiments/test_task/images/102.png"
+];
+subjcodeArray[336] = "arctic";
+
+
+picArray[356] = ["static/experiments/test_task/images/6.png",
+  "static/experiments/test_task/images/28.png",
+  "static/experiments/test_task/images/10.png",
+  "static/experiments/test_task/images/26.png",
+  "static/experiments/test_task/images/29.png",
+  "static/experiments/test_task/images/20.png",
+  "static/experiments/test_task/images/11.png",
+  "static/experiments/test_task/images/19.png",
+  "static/experiments/test_task/images/101.png",
+	"static/experiments/test_task/images/102.png"
+];
+subjcodeArray[356] = "plane";
+
+
+picArray[358] = ["static/experiments/test_task/images/28.png",
+  "static/experiments/test_task/images/17.png",
+  "static/experiments/test_task/images/14.png",
+  "static/experiments/test_task/images/19.png",
+  "static/experiments/test_task/images/32.png",
+  "static/experiments/test_task/images/37.png",
+  "static/experiments/test_task/images/31.png",
+  "static/experiments/test_task/images/10.png",
+  "static/experiments/test_task/images/101.png",
+	"static/experiments/test_task/images/102.png"
+];
+subjcodeArray[358] = "apple";
+
+
+picArray[359] = ["static/experiments/test_task/images/21.png",
+  "static/experiments/test_task/images/27.png",
+  "static/experiments/test_task/images/22.png",
+  "static/experiments/test_task/images/32.png",
+  "static/experiments/test_task/images/20.png",
+  "static/experiments/test_task/images/11.png",
+  "static/experiments/test_task/images/37.png",
+  "static/experiments/test_task/images/28.png",
+  "static/experiments/test_task/images/101.png",
+	"static/experiments/test_task/images/102.png"
+];
+subjcodeArray[359] = "tiger";
+
+
+picArray[361] = ["static/experiments/test_task/images/17.png",
+  "static/experiments/test_task/images/35.png",
+  "static/experiments/test_task/images/2.png",
+  "static/experiments/test_task/images/24.png",
+  "static/experiments/test_task/images/18.png",
+  "static/experiments/test_task/images/30.png",
+  "static/experiments/test_task/images/22.png",
+  "static/experiments/test_task/images/34.png",
+  "static/experiments/test_task/images/101.png",
+	"static/experiments/test_task/images/102.png"
+];
+subjcodeArray[361] = "travel";
+
+
+picArray[951] = ["static/experiments/test_task/images/3.png",
+  "static/experiments/test_task/images/29.png",
+  "static/experiments/test_task/images/1.png",
+  "static/experiments/test_task/images/38.png",
+  "static/experiments/test_task/images/37.png",
+  "static/experiments/test_task/images/16.png",
+  "static/experiments/test_task/images/24.png",
+  "static/experiments/test_task/images/10.png",
+  "static/experiments/test_task/images/101.png",
+	"static/experiments/test_task/images/102.png"
+];
+subjcodeArray[951] = "university";
+
+
+picArray[366] = ["static/experiments/test_task/images/28.png",
+  "static/experiments/test_task/images/4.png",
+  "static/experiments/test_task/images/38.png",
+  "static/experiments/test_task/images/14.png",
+  "static/experiments/test_task/images/11.png",
+  "static/experiments/test_task/images/32.png",
+  "static/experiments/test_task/images/13.png",
+  "static/experiments/test_task/images/27.png",
+  "static/experiments/test_task/images/101.png",
+	"static/experiments/test_task/images/102.png"
+];
+subjcodeArray[366] = "pingpong";
 
