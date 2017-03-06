b5174e9def87212a62863b4b9e70d09b03f94058
gewimmer <gelliottwimmer@stanford.edu>
2017-03-06 14:03:24 -0800

    Fixing list.js part 2.

--- reward_learning/list.js
+++ reward_learning/list.js
@@ -1078,9 +1078,21 @@ picArray[260] = ["/static/experiments/test_task/images/14.png",
   "/static/experiments/test_task/images/13.png",
   "/static/experiments/test_task/images/101.png",
 	"/static/experiments/test_task/images/102.png"
-];  
+];
 subjcodeArray[260] = "orchid";
 
 
+picArray[527] = ["/static/experiments/test_task/images/14.png",
+  "/static/experiments/test_task/images/18.png",
+  "/static/experiments/test_task/images/31.png",
+  "/static/experiments/test_task/images/37.png",
+  "/static/experiments/test_task/images/28.png",
+  "/static/experiments/test_task/images/11.png",
+  "/static/experiments/test_task/images/5.png",
+  "/static/experiments/test_task/images/13.png",
+  "/static/experiments/test_task/images/101.png",
+	"/static/experiments/test_task/images/102.png"
+];
+subjcodeArray[527] = "bowie";
 
 
