68a2d54588888818056059eeac4fc1c027866414
gewimmer <gelliottwimmer@stanford.edu>
2017-03-06 14:34:33 -0800

    Fixing list.js.

--- reward_learning/list.js
+++ reward_learning/list.js
@@ -31,6 +31,48 @@ picArray[250] = ["/static/experiments/test_task/images/1.png",
 subjcodeArray[250] = "rainbow";
 
 
+picArray[445] = ["/static/experiments/test_task/images/14.png",
+  "/static/experiments/test_task/images/18.png",
+  "/static/experiments/test_task/images/31.png",
+  "/static/experiments/test_task/images/37.png",
+  "/static/experiments/test_task/images/28.png",
+  "/static/experiments/test_task/images/11.png",
+  "/static/experiments/test_task/images/5.png",
+  "/static/experiments/test_task/images/13.png",
+  "/static/experiments/test_task/images/101.png",
+	"/static/experiments/test_task/images/102.png"
+];  
+subjcodeArray[445] = "magnolia";
+
+
+picArray[521] = ["/static/experiments/test_task/images/14.png",
+  "/static/experiments/test_task/images/18.png",
+  "/static/experiments/test_task/images/31.png",
+  "/static/experiments/test_task/images/37.png",
+  "/static/experiments/test_task/images/28.png",
+  "/static/experiments/test_task/images/11.png",
+  "/static/experiments/test_task/images/5.png",
+  "/static/experiments/test_task/images/13.png",
+  "/static/experiments/test_task/images/101.png",
+	"/static/experiments/test_task/images/102.png"
+];  
+subjcodeArray[521] = "tulip";
+
+
+picArray[260] = ["/static/experiments/test_task/images/14.png",
+  "/static/experiments/test_task/images/18.png",
+  "/static/experiments/test_task/images/31.png",
+  "/static/experiments/test_task/images/37.png",
+	"/static/experiments/test_task/images/28.png",
+	"/static/experiments/test_task/images/11.png",
+	"/static/experiments/test_task/images/5.png",
+	"/static/experiments/test_task/images/13.png",
+	"/static/experiments/test_task/images/101.png",
+	"/static/experiments/test_task/images/102.png"
+];
+subjcodeArray[260] = "orchid";
+
+
 picArray[527] = ["/static/experiments/test_task/images/14.png",
   "/static/experiments/test_task/images/18.png",
   "/static/experiments/test_task/images/31.png",
