48d00694c69c99608be2c86bf0c43bae2309358a
Jamie Li <jamie@DN0a22166c.SUNet>
2018-11-09 13:21:38 -0800

    flanker single task - ready for full length run

--- flanker_single_task_network/experiment.js
+++ flanker_single_task_network/experiment.js
@@ -33,7 +33,7 @@ function assessPerformance() {
 		choice_counts[choices[k]] = 0
 	}
 	for (var i = 0; i < experiment_data.length; i++) {
-		if (experiment_data[i].trial_id == 'test_trial') {
+		if ((experiment_data[i].trial_id == 'test_trial') || (experiment_data[i].trial_id == 'practice_trial')) {
 			trial_count += 1
 			rt = experiment_data[i].rt
 			key = experiment_data[i].key_press
