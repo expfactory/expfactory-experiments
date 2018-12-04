1e69f9e0f3e3d11f49f227279ce3257c8ba7e70f
Jamie Li <jamie@jamies-mbp.attlocal.net>
2018-12-03 16:02:37 -0800

    stop_signal circleci

--- stop_signal/experiment.js
+++ stop_signal/experiment.js
@@ -1,6 +1,10 @@
 /* ************************************ */
 /* Define helper functions */
 /* ************************************ */
+function addID() {
+  jsPsych.data.addDataToLastTrial({exp_id: 'stop_signal'})
+}
+
 function evalAttentionChecks() {
 	var check_percent = 1
 	if (run_attention_checks) {
@@ -325,7 +329,6 @@ var end_block = {
 	type: 'poldrack-text',
 	data: {
 		trial_id: "end",
-    	exp_id: 'stop_signal'
 	},
 	timing_response: 180000,
 	text: '<div class = centerbox><p class = center-block-text>Thanks for completing this task!</p><p class = center-block-text>Press <strong>enter</strong> to continue.</p></div>',
