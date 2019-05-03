fdd6133ddd886e53f9aa2a37b3d0c9a77238999a
Jamie Li <jamie@jamies-mbp-2.attlocal.net>
2019-05-03 15:38:51 -0700

    deleted other browser checks that are not chrome.

--- test_browser_chrome/experiment.js
+++ test_browser_chrome/experiment.js
@@ -12,7 +12,7 @@ function addID() {
 
 
 var getFeedback = function() {
-	if (isChrome == true){
+	if (isChrome === true){
 		feedback_text += 
 			'</p><p class = block-text>You are using a Chrome browser. Please finish this task and continue on with the rest of the battery!'
 		feedback_text += 
@@ -55,25 +55,8 @@ var fileTypePNG = ".png'></img>"
 var preFileType = "<img class = center src='/static/experiments/test_browser_chrome/images/"
 
 
-var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-
-// Firefox 1.0+
-var isFirefox = typeof InstallTrigger !== 'undefined';
-
-// Safari 3.0+ "[object HTMLElementConstructor]" 
-var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
-
-// Internet Explorer 6-11
-var isIE = /*@cc_on!@*/false || !!document.documentMode;
-
-// Edge 20+
-var isEdge = !isIE && !!window.StyleMedia;
-
 // Chrome 1 - 71
 var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
-
-// Blink engine detection
-var isBlink = (isChrome || isOpera) && !!window.CSS;
 
 
 /* ************************************ */
